264b89233ec1217053a9ebb55e7211aff8b2545d
Alan da Costa Santos <alancostasantos07@gmail.com>
2020-12-08 12:12:26 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AulaPOO_Heranca
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
